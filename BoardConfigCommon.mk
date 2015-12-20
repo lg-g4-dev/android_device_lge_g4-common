9c96adad1769fe0a3b254e061bdbb60b53d96c53
genesixx <erwanleboucher@gmail.com>
2015-12-20 12:28:08 +0100

    build in permissive

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,7 +47,7 @@ TARGET_USES_C2D_COMPOSITION := true
 
 # Kernel
 BOARD_DTBTOOL_ARGS := -2
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00078000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x02000000
@@ -159,7 +159,7 @@ TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
